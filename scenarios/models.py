9cd368251b31d4d2b82eac07689f3e7c7ed2c363
Ryan Alexander <ryandalex1@outlook.com>
2020-07-21 13:31:44 -0500

    Refactor expansions to their own Model

--- scenarios/models.py
+++ scenarios/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
 
+class ExpansionManager(models.Manager):
+    def get_by_natural_key(self, abbreviation):
+        return self.get(abbreviation=abbreviation)
+
+
+class Expansion(models.Model):
+    name = models.CharField(max_length=50)
+    abbreviation = models.CharField(max_length=25, unique=True)
+
+    objects = ExpansionManager()
+
+
 class Scenario(models.Model):
     name = models.CharField(max_length=50)
-    expansion = models.CharField(max_length=15)
+    expansion = models.ForeignKey(Expansion, on_delete=models.CASCADE)
     number = models.CharField(max_length=15)
 
     board_types = (
