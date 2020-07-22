a90ae806b4e5c76145d25e8c766687c5de33abb2
Ryan Alexander <ryandalex1@outlook.com>
2020-07-22 11:24:38 -0500

    Basic sign in/sign out pages and functionality

--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 
 from user_profile.forms import SignUpForm
+
+from django.contrib.auth.forms import AuthenticationForm
 
 
 def index(request):
@@ -18,3 +20,26 @@ def sign_up(request):
             return redirect('index')
     context['form'] = form
     return render(request, 'registration/sign_up.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect("index")
+
+
+def login_user(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+
+    context = {}
+    form = AuthenticationForm()
+    context['form'] = form
+    return render(request, "registration/login.html", context)
--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from core import views
 
+app_name = 'user_profile'
+
 urlpatterns = [
-    path('sign_up/', views.sign_up, name="sign-up")
+    path('sign_up/', views.sign_up, name="sign_up"),
+    path('logout/', views.logout_user, name="logout"),
+    path('login/', views.login_user, name="login")
 ]
