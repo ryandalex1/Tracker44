f55569b2025828c2e591ac01143282b954cafc4e
Ryan Alexander <ryandalex1@outlook.com>
2020-07-23 12:32:15 -0500

    Add 4077th font to homepage and header

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -6,7 +6,10 @@ import user_profile.views
 app_name = 'user_profile'
 
 urlpatterns = [
+    # /user_profile/sign_up
     path('sign_up/', user_profile.views.sign_up, name="sign_up"),
+    # /user_profile/logout
     path('logout/', user_profile.views.logout_user, name="logout"),
+    # /user_profile/login
     path('login/', user_profile.views.login_user, name="login")
 ]
