5153201a073b757fe4c9aae515c3f334d2935d4e
Ryan Alexander <ryandalex1@outlook.com>
2020-07-24 15:37:55 -0500

    Load scenarios from the database and add sub_expansion model

--- scenarios/models.py
+++ scenarios/models.py
@@ -6,6 +6,11 @@ class ExpansionManager(models.Manager):
         return self.get(abbreviation=abbreviation)
 
 
+class SubExpansionManager(models.Manager):
+    def get_by_natural_key(self, abbreviation):
+        return self.get(abbreviation=abbreviation)
+
+
 class Expansion(models.Model):
     name = models.CharField(max_length=50)
     abbreviation = models.CharField(max_length=25, unique=True)
@@ -13,15 +18,25 @@ class Expansion(models.Model):
     objects = ExpansionManager()
 
 
+class SubExpansion(models.Model):
+    name = models.CharField(max_length=50)
+    abbreviation = models.CharField(max_length=25, unique=True)
+    parent = models.ForeignKey(Expansion, on_delete=models.CASCADE)
+
+    objects = SubExpansionManager()
+
+
 class Scenario(models.Model):
     name = models.CharField(max_length=50)
     expansion = models.ForeignKey(Expansion, on_delete=models.CASCADE)
+    sub_expansion = models.ForeignKey(SubExpansion, on_delete=models.CASCADE, blank=True, null=True)
     number = models.CharField(max_length=15)
+    date = models.CharField(max_length=25)
 
     board_types = (
         (1, "Beach"),
         (2, "Winter"),
         (3, "Desert"),
-        (4, "Field")
+        (4, "Countryside")
     )
     board = models.IntegerField(choices=board_types)
--- scenarios/views.py
+++ scenarios/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
+from .models import Scenario
+
 
 def expansion_list(request):
     return render(request, 'scenarios/expansion_list.html')
 
 
 def all_scenarios(request):
-    return render(request, 'scenarios/all_scenarios.html')
+    context = {'scenarios': Scenario.objects.all()}
+    return render(request, 'scenarios/all_scenarios.html', context)
 
