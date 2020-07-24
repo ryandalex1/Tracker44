75e7439e06a895eb7a0603020241c72a051acb85
Ryan Alexander <ryandalex1@outlook.com>
2020-07-24 14:58:14 -0500

    Create basic proof of concept with DataTables and Bootstrap for displaying scenarios

--- scenarios/urls.py
+++ scenarios/urls.py
@@ -8,4 +8,6 @@ app_name = 'scenarios'
 urlpatterns = [
     # /scenarios/
     path('', scenarios.views.expansion_list, name='expansions'),
+    # /scenarios/all
+    path('all/', scenarios.views.all_scenarios, name='all_scenarios'),
 ]
--- scenarios/views.py
+++ scenarios/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def expansion_list(request):
     return render(request, 'scenarios/expansion_list.html')
+
+
+def all_scenarios(request):
+    return render(request, 'scenarios/all_scenarios.html')
+
